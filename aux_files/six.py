fc5279b5c2a20cab4f39534141253564f853cb79
julioaamaral <julioamaral@ncc.unesp.br>
2017-06-26 19:22:25 -0300

    Delete six.py

--- aux_files/six.py
+++
@@ -1,8 +0,0 @@
-...
-...
-model = Sequential()
-model.add(Dense(32, activation='relu', input_dim=100))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(optimizer='rmsprop',loss='binary_crossentropy',
-              metrics=['accuracy'])
-model.fit(data, labels, epochs=10, batch_size=32)
