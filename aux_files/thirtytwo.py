e5fe8e2065e1ce6271c254eb7f3791829875ebfd
julioaamaral <julioamaral@ncc.unesp.br>
2017-06-26 19:29:00 -0300

    Delete thirtytwo.py

--- aux_files/thirtytwo.py
+++
@@ -1,9 +0,0 @@
-         # CONV => RELU => POOL
-         model.add(Conv2D(50, kernel_size=5, border_mode="same"))
-         model.add(Activation("relu"))
-         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-         # Flatten => RELU layers
-         model.add(Flatten())
-         model.add(Dense(500))
-         model.add(Activation("relu"))
-...
