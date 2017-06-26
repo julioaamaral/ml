4917973072a64c9846bf4224ce1fd85a12202609
julioaamaral <julioamaral@ncc.unesp.br>
2017-06-26 19:19:38 -0300

    Delete julio.py

--- aux_files/julio.py
+++
@@ -1,5 +0,0 @@
-from keras.layers.core import Dense, Activation
-from keras.optimizers import SGD
-from keras.utils import np_utils
-np.random.seed(1671)  # for reproducibility
-model.add(Activation('softmax'))
