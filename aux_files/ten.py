8b28fd2ef79511836a5a253f9b7a7061728ef86c
julioaamaral <julioamaral@ncc.unesp.br>
2017-06-26 19:25:42 -0300

    Delete ten.py

--- aux_files/ten.py
+++
@@ -1,9 +0,0 @@
-...
-model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=20))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
-...
-
